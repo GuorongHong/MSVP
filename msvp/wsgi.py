b2f57dde9c0c55c73ac69cad60d2c3de84668814
GR <hongguorong97@gmail.com>
2019-06-18 22:54:16 +0800

    minor fix in wsgi

--- msvp/wsgi.py
+++ msvp/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'msvp.settings')
 
 application = get_wsgi_application()
 
