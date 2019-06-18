544d85e2ae4c77e0c5b21b03f565b09919b19c96
suwoons <sarahlim2612@gmail.com>
2019-06-18 16:41:02 +0800

    fixed validation error for generating pw

--- password/forms.py
+++ password/forms.py
@@ -1,9 +1,9 @@
 from django import forms 
 
 class PasswordForm(forms.Form):
-    userid = forms.CharField(label='UserID', max_length = 200)
-    pw = forms.CharField(label='Password', max_length = 200)
-    web = forms.CharField(label='Website', max_length = 200)
+    userid = forms.CharField(label='UserID', max_length = 200, widget=forms.TextInput(attrs={"placeholder":"Your UserID"}))
+    pw = forms.CharField(label='Password', max_length = 200, widget=forms.TextInput(attrs={"placeholder":"Your Password"}))
+    web = forms.CharField(label='Website', max_length = 200, widget=forms.TextInput(attrs={"placeholder":"Website title"}))
 
 class GeneratePasswordForm(forms.Form):
     length = forms.IntegerField(label='Length', min_value = 4, max_value = 128)
@@ -11,15 +11,13 @@ class GeneratePasswordForm(forms.Form):
     use_lower = forms.BooleanField(label="Lowercase", initial = True, required = False)
     use_digits = forms.BooleanField(label="Digits", initial = True, required = False)
     use_special = forms.BooleanField(label="Special characters", initial = True, required = False)
-    avoid_similar = forms.BooleanField(label="Avoid similar letters and numbers", initial = True, required = False)
+    avoid_similar = forms.BooleanField(label="Avoid similar letters and numbers (Eg. 0 and O)", initial = True, required = False)
     
-    def validation_message(self):
-        length = self.clean_data.get('length', '')
-        upper = self.clean_data.get('use_upper', '')
-        lower = self.clean_data.get('use_lower', '')
-        digits = self.clean_data.get('use_digits', '')
-        special = self.clean_data.get('use_special', '')
-        avoid_similar = self.clean_data.get('avoid_similar', '')
-
-        if length or upper or lower or digits or special or avoid_similar:
+    def clean(self):
+        upper = self.cleaned_data['use_upper']
+        lower = self.cleaned_data['use_lower']
+        digits = self.cleaned_data['use_digits']
+        special = self.cleaned_data['use_special']
+        avoid_similar = self.cleaned_data['avoid_similar']
+        if upper == lower == digits == special == avoid_similar == False:
              raise forms.ValidationError("Please select at least one.")
