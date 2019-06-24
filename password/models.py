04e69e2a64587e3f13f695e9f2afa6900b43804c
suwoons <sarahlim2612@gmail.com>
2019-06-24 22:22:26 +0800

    edited models

--- password/models.py
+++ password/models.py
@@ -3,9 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
 class Passwords(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     userid = models.CharField(max_length = 200)
