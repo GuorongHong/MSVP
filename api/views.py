43c786205ddf9700bf00f21b6c9c0914eef26d92
suwoons <sarahlim2612@gmail.com>
2019-07-10 14:38:06 +0800

    edit api

--- api/views.py
+++ api/views.py
@@ -1,9 +1,19 @@
+from django.contrib.auth import login
+
 from rest_framework import generics
+from rest_framework import authentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from password import models
 from . import serializers
 
 class ListPassword(generics.ListCreateAPIView):
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     queryset = models.Passwords.objects.all()
     serializer_class = serializers.PasswordSerializer
 
--- msvp/settings.py
+++ msvp/settings.py
@@ -151,6 +151,10 @@ STATICFILES_DIRS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ]
