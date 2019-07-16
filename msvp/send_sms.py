590b9ae8282a4fa93b6e2c2d96e824ada55f9ff4
GR <hongguorong97@gmail.com>
2019-07-16 18:02:24 +0800

    2FA

---
+++ msvp/send_sms.py
@@ -0,0 +1,16 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'AC4957736b266d4bb29d9777e1f7872ef9'
+auth_token = 'c3695221ed838baebbf6828d30039c37'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+15017122661',
+                     to='+15558675310'
+                 )
