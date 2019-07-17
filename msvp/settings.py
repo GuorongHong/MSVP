3d3b42fb35a8d2d39ea6a88218df0ecee01f8ae0
GR <hongguorong97@gmail.com>
2019-07-17 16:14:02 +0800

    modified settings

--- msvp/settings.py
+++ msvp/settings.py
@@ -144,8 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
