8d883ec20ed776228b5656b78f85c4264d3ffe9a
GR <hongguorong97@gmail.com>
2019-07-23 01:02:31 +0800

    removed static files

--- msvp/settings.py
+++ msvp/settings.py
@@ -95,27 +95,27 @@ WSGI_APPLICATION = 'msvp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'msvp',
-#         'USER': 'grhong',
-#         'PASSWORD': 'test1',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'msvp',
-        'USER': 'sarah',
+        'USER': 'grhong',
         'PASSWORD': 'test1',
         'HOST': 'localhost',
-        'PORT': '5433',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'msvp',
+#         'USER': 'sarah',
+#         'PASSWORD': 'test1',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
+#     }
+# }
 
 
 # Password validation
