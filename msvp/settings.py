e5b4370932e92d5f2c8b0c7ce428c6c110c7f561
GR <hongguorong97@gmail.com>
2019-07-23 18:18:34 +0800

    edited settings

--- msvp/settings.py
+++ msvp/settings.py
@@ -158,7 +158,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'msvp/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
